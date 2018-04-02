534f0b31e5418b00d53e07c50e0e4a60dc1a11d0
huntergai <gaizuojia@gmail.com>
2018-04-03 00:17:14 +0800

    part2 second test

--- PersonalMsg.py
+++ PersonalMsg.py
@@ -7,3 +7,5 @@ name2 = "huntergai"
 print("Hello " + name2.upper() + ",would you like to learn some Python today?")
 print("Hello " + name2.lower() + ",would you like to learn some Python today?")
 print("Hello " + name2.title() + ",would you like to learn some Python today?")
+
+
---
+++ quotes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+
+famous_person = "Albert Einstein"
+message = famous_person + ' once said, "A person who never made a mistake never tried anything new."'
+print(message)
+
+famous_person2 = " Albert Einstein "
+
+print(famous_person2.lstrip())
+print(famous_person2.rstrip())
+print(famous_person2.strip())
