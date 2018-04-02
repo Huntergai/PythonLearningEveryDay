9327da42de0b660f18f2c729cc9616c9d818bb49
huntergai <gaizuojia@gmail.com>
2018-04-03 00:10:00 +0800

    second test

---
+++ PersonalMsg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+name = "hunter"
+print("Hello " + name + ",would you like to learn some Python today?")
+
+name2 = "huntergai"
+print("Hello " + name2.upper() + ",would you like to learn some Python today?")
+print("Hello " + name2.lower() + ",would you like to learn some Python today?")
+print("Hello " + name2.title() + ",would you like to learn some Python today?")
